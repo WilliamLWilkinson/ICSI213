4fa213712e76972a75c6ecaea92c289d7bd7b2b4
WilliamLWilkinson <wwilkinson@albany.edu>
2017-02-07 21:07:17 -0500

    Wrote Junit test for testing setName method for item class

--- HW_01/src/Tests/ItemTests.java
+++ HW_01/src/Tests/ItemTests.java
@@ -4,11 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Code.Item;
+
+
 public class ItemTests {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
-	}
+		Item one = new Item();
+		one.setName("Test item");
+		String actual = one.getName();
+		String expected = "Test item";
+		
+		assertTrue("I expect th name of the item to be "+
+					expected+ " but it was actually" + actual, new String(actual).equals(expected) );
+		
+	}//end of test
+	
+	@Test
+	public void test1(){
+		
+	}// end of test1
 
-}
+}//End of test class
