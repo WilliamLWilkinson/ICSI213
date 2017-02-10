8be75c7d285786e6d4d7041be6ee0479c8efcdbd
WilliamLWilkinson <wwilkinson@albany.edu>
2017-02-09 20:26:50 -0500

    Adds JUnit test for getWeight and setWeight

--- HW_01/src/Tests/ItemTests.java
+++ HW_01/src/Tests/ItemTests.java
@@ -9,21 +9,31 @@ import Code.Item;
 
 public class ItemTests {
 
+	// Tests setName and getName method
 	@Test
 	public void test() {
+		
 		Item one = new Item();
 		one.setName("Test item");
 		String actual = one.getName();
 		String expected = "Test item";
 		
 		assertTrue("I expect th name of the item to be "+
-					expected+ " but it was actually" + actual, new String(actual).equals(expected) );
+					expected+ " but it was actually " + actual, new String(actual).equals(expected) );
 		
 	}//end of test
 	
+	// tests getWeight and setWeight method
 	@Test
 	public void test1(){
+		Item testItem = new Item();
+		testItem.setWeight(10);
+		int actual = testItem.getWeight();
+		int expected = 10;
 		
+		assertTrue("I expect the weight of the item to be "
+					+ Integer.toString(expected) + " but was actually " 
+					+ Integer.toString(actual), actual==expected);
 	}// end of test1
 
 }//End of test class
