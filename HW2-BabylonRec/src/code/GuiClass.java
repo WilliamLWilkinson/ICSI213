3de800e165662d8c8c9e741199b2e3fe659776c2
WilliamLWilkinson <wwilkinson@albany.edu>
2017-02-15 20:11:05 -0500

    Adds class Driver, SquareRoot, and adds documentation to GuiClass

--- HW2-BabylonRec/src/code/GuiClass.java
+++ HW2-BabylonRec/src/code/GuiClass.java
@@ -7,19 +7,19 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class GuiClass implements Runnable {
-	private String _instructionOne;
-	private String _instructionTwo;
-	private JFrame _rootWindow;
-	private JPanel _buttonPanel;
-	private JPanel _instrucPanel;
-	private JButton _calcB;
-	private JButton _resetB;
-	private JLabel _directionsSRN;
-	private JLabel _directionsPA;
-	private TextField _numSqaure;
-	private TextField _percAccuracy;
+	private String _instructionOne;	//The first instruction for the user
+	private String _instructionTwo;	//The second instruction for the user
+	private JFrame _rootWindow;		//JFrame of the Babylonian square root gui 
+	private JPanel _buttonPanel;	//JPanel to contain the buttons for calculations and reset
+	private JPanel _instrucPanel;	//JPanel that contains the instructions and textfields
+	private JButton _calcB;			//JButton which will calculate the 
+	private JButton _resetB;		//JButton that will reset the gui to its beginning state
+	private JLabel _directionsSRN; 	//
+	private JLabel _directionsPA;	//
+	private TextField _numSqaure;	//Textfield user will enter teh number they want to find the square root of 
+	private TextField _percAccuracy;//Textfield user enters accuracy 
 
-	
+	// GuiClass default constructor
 	public GuiClass(){
 		_instructionOne = "Please enter the number you wish to find the square rood of";
 		_instructionTwo = "Please enter the percent accuracy without the percent symbol";
@@ -28,13 +28,22 @@ public class GuiClass implements Runnable {
 		_instrucPanel = new JPanel();
 		_calcB = new JButton("Find Square root");
 		_resetB = new JButton("Reset");
-		_directionsSRN = new JLabel();
-		_directionsPA = new JLabel();
+		_directionsSRN = new JLabel(_instructionOne);	//Adds instructionsOne to JLabel
+		_directionsPA = new JLabel(_instructionTwo);	//Adds instructionTwo to JLabel
 		_numSqaure = new TextField();
 		 _percAccuracy = new TextField();
 		 
 		 _rootWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		 
+		 //Adds button to button panel
+		 _buttonPanel.add(_calcB);
+		 _buttonPanel.add(_resetB);
+		 
+		 //Adds 
+		 //Adds panels to JFrame
+		 _rootWindow.add(_buttonPanel);
+		 _rootWindow.add(_buttonPanel);
+	}// end of GuiClass()
 	
 	
 	@Override
