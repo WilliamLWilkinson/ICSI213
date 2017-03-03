d459d740e9768ad1e4c737cb12b88ead8afc4762
WilliamLWilkinson <wwilkinson@albany.edu>
2017-03-02 19:55:40 -0500

    Adds constructors to DoublyList class

--- HW03_DoublyLinkedList/src/code/DoublyList.java
+++ HW03_DoublyLinkedList/src/code/DoublyList.java
@@ -1,5 +1,23 @@
 package code;
 
 public class DoublyList {
+	private int _size;
+	private Node _head;
+	private Node _tail;
+	
+	//Default constructor
+	public DoublyList(){
+		_size =0;
+		_head =null;
+		_tail = null;
+	}
+	
+	//Non-default constructor 
+	public DoublyList(Node newNode){
+		_head = newNode;
+		_tail = newNode;
+		_size = 1;
+	}
 
+	
 }
