c50a231537b51037be5d3f8868a1282f180b7b26
WilliamLWilkinson <wwilkinson@albany.edu>
2017-03-09 08:34:35 -0500

    Adds findNode method

--- HW03_DoublyLinkedList/src/code/DoublyList.java
+++ HW03_DoublyLinkedList/src/code/DoublyList.java
@@ -28,11 +28,19 @@ public class DoublyList {
 	public void insertNodeAlphabetically(String s){
 		Node newNode = new Node(s);
 		
+		//If the list is empty set the tail and head to the argument string 
 		if(isEmpty()){
 			_head = newNode;
 			_tail =newNode;
 			_size++;
-		}
+		}//end of if
+		//Else if the list is not empty
+		else{
+			Node temp = _head;
+			while(temp !=null){
+				
+			}//end of while
+		}//end of else
 		
 	}//end of insertNode
 	
@@ -51,7 +59,7 @@ public class DoublyList {
 			System.out.println(temp.getDate());
 			//Get next node
 			temp = temp.getNext();
-		}
+		}//end of while
 	}//end of print forward
 	
 	//Prints the list starting from the back
@@ -63,7 +71,7 @@ public class DoublyList {
 			System.out.println(temp.getDate());
 			//Gets previous node
 			temp = temp.getPrev();
-		}
+		}//end of while
 	}// end of print backwards
 	
 	//Deletes the list
@@ -73,4 +81,27 @@ public class DoublyList {
 		_tail = null;
 		_size=0;
 	}//end of deleteList()
+	
+	//Traverses the list and tells the user if the input string was found
+	//and at which location
+	public void findNode(String target){
+		Node temp = _head;
+		int location =0;
+		//While there are 
+		while(location <_size){
+			if(temp.getDate().compareTo(target)==0){
+				System.out.println("String: " + target + "was found in the: "+ (location +1) + "index");
+				location = _size;//Breaks the loop
+			}
+			//The item was not found
+			else if(location == _size){
+				System.out.println("String: " + target + "was not found");
+			}
+			//The string was not found but location is smaller than size
+			else{
+				location++;
+				temp = temp.getNext();
+			}
+		}//end of while
+	}//end of find node
 }
