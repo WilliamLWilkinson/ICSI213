dbbe8c5dbbd10b83d9fe7085af517919021452c0
WilliamLWilkinson <wwilkinson@albany.edu>
2017-03-25 20:03:15 -0400

    adds addToFront method and tests it in main

--- HW4_StacksAndQueues/src/code/LinkedList.java
+++ HW4_StacksAndQueues/src/code/LinkedList.java
@@ -1,5 +1,58 @@
 package code;
 
-public class LinkedList {
+public class LinkedList<E> {
+	
+	//Node class
+	private static class Node<E> {
+		public E item;
+		public Node<E> prev;
+		public Node<E> next;
+		public Node(Node<E> prev, E element, Node<E> next) {
+			this.item = element;
+			this.prev = prev;
+			this.next = next;
+		}
+	}//end of node class
 
+	private Node<E> _head;
+	private Node<E> _tail;
+	private int _size;
+	
+	public LinkedList(){
+		_head = null;
+		_tail = null;
+		_size = 0;
+	}
+	
+	//
+	public boolean isEmpty(){
+		return _size ==0;
+	}//end of isEmpty
+	
+	
+	public void addToFront(E e){
+		
+		Node<E> newNode = new Node<E>(null, e,null);
+		if(_size ==0){
+			_head = newNode;
+			_tail = newNode;
+		}
+		else{
+			Node<E> temp = _head;
+			newNode.next = temp;
+			temp.prev = newNode;
+			_head = newNode;
+		}
+		_size++;
+	}//end of addToFront
+	
+	public void print(){
+		Node<E> temp = _head;
+		
+		for(int i =0; i<_size;i++){
+			System.out.println(temp.item);
+			temp = temp.next;
+		}
+		
+	}//end of print
 }
--- HW4_StacksAndQueues/src/code/Main.java
+++ HW4_StacksAndQueues/src/code/Main.java
@@ -2,4 +2,15 @@ package code;
 
 public class Main {
 
+	 public static void main(String[] args) {	
+		 				
+		 LinkedList one = new LinkedList();
+		 one.addToFront("Billy");
+		 one.print();
+		 one.addToFront("Matt");
+		 
+		 one.print();
+		 
+		 
+	 }
 }
