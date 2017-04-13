881fc93fdedfd6fab4fd459b1aaea6546c72d607
WilliamLWilkinson <wwilkinson@albany.edu>
2017-04-13 18:03:12 -0400

    Adds bubbleSortArray method which sorts an array list using bubble sort
    method

--- HW05_BubbleSorting/src/code/Main.java
+++ HW05_BubbleSorting/src/code/Main.java
@@ -6,25 +6,35 @@ public class Main {
 	public static void main(String[] args) {	
 		int _sizeOfList = 10;
 		int arrayOne[] = new int[_sizeOfList];
-		
+		fillArray(arrayOne);
+		//Prints the list before it is sorted
+		printArray(arrayOne);
 		
 	}//end of main method
 	
 	//Method fillArray takes in an array of integers and 
-	//fills it with integers
+	//fills it with integers between 0 and 100
 	public static void fillArray(int a[]){
 		
 		for(int i =0;i<a.length;i++){
 			Random rand = new Random();
 			
-			int ranNum = rand.nextInt(0-100 +1)+0;
+			int ranNum = rand.nextInt(100-0)+0;
 			a[i]= ranNum;
 		}
 	}//end of fillArray
 	
-	public static void printArrray(int a[]){
+	//Prints the contents of an Array
+	public static void printArray(int a[]){
 		for(int i=0;i<a.length;i++){
 			System.out.println(Integer.toString(a[i]));
 		}
+	}//end of printArray
+	
+	//Sorts the contents of an Array using the bubble sort method
+	public static void bubbleSortArray(){
+		
+		
 	}
+	
 }//end of main class
